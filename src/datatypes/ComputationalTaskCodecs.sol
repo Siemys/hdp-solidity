c2f28c71533da4b98da0e74b883a8bb854711a99
Pia <gayeongparkk@gmail.com>
2024-04-11 14:14:13 +0900

    update test sync e6c8896, forge fmt

--- src/datatypes/ComputationalTaskCodecs.sol
+++ src/datatypes/ComputationalTaskCodecs.sol
@@ -38,48 +38,26 @@ enum Operator {
 library ComputationalTaskCodecs {
     /// @dev Encodes a ComputationalTask.
     /// @param task The ComputationalTask to encode.
-    function encode(
-        ComputationalTask memory task
-    ) internal pure returns (bytes memory) {
-        return
-            abi.encode(
-                task.aggregateFnId,
-                task.operatorId,
-                task.valueToCompare
-            );
+    function encode(ComputationalTask memory task) internal pure returns (bytes memory) {
+        return abi.encode(task.aggregateFnId, task.operatorId, task.valueToCompare);
     }
 
     /// @dev Get the commitment of a ComputationalTask.
     /// @notice The commitment embeds the datalake commitment.
     /// @param task The ComputationalTask to commit.
     /// @param datalakeCommitment The commitment of the datalake.
-    function commit(
-        ComputationalTask memory task,
-        bytes32 datalakeCommitment
-    ) internal pure returns (bytes32) {
-        return
-            keccak256(
-                abi.encode(
-                    datalakeCommitment,
-                    task.aggregateFnId,
-                    task.operatorId,
-                    task.valueToCompare
-                )
-            );
+    function commit(ComputationalTask memory task, bytes32 datalakeCommitment) internal pure returns (bytes32) {
+        return keccak256(abi.encode(datalakeCommitment, task.aggregateFnId, task.operatorId, task.valueToCompare));
     }
 
     /// @dev Decodes a ComputationalTask.
     /// @param data The encoded ComputationalTask.
-    function decode(
-        bytes memory data
-    ) internal pure returns (ComputationalTask memory) {
-        (uint8 aggregateFnId, uint8 operator, uint256 valueToCompare) = abi
-            .decode(data, (uint8, uint8, uint256));
-        return
-            ComputationalTask({
-                aggregateFnId: AggregateFn(aggregateFnId),
-                operatorId: Operator(operator),
-                valueToCompare: valueToCompare
-            });
+    function decode(bytes memory data) internal pure returns (ComputationalTask memory) {
+        (uint8 aggregateFnId, uint8 operator, uint256 valueToCompare) = abi.decode(data, (uint8, uint8, uint256));
+        return ComputationalTask({
+            aggregateFnId: AggregateFn(aggregateFnId),
+            operatorId: Operator(operator),
+            valueToCompare: valueToCompare
+        });
     }
 }
--- src/interfaces/IAggregatorsFactory.sol
+++ src/interfaces/IAggregatorsFactory.sol
@@ -5,12 +5,7 @@ import {ISharpFactsAggregator} from "./ISharpFactsAggregator.sol";
 
 /// @notice Aggregators factory interface.
 interface IAggregatorsFactory {
-    function createAggregator(
-        uint256 id,
-        ISharpFactsAggregator aggregator
-    ) external;
+    function createAggregator(uint256 id, ISharpFactsAggregator aggregator) external;
 
-    function aggregatorsById(
-        uint256 id
-    ) external view returns (ISharpFactsAggregator);
+    function aggregatorsById(uint256 id) external view returns (ISharpFactsAggregator);
 }
