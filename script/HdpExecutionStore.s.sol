3bf4f8aa5a5ccecc7e3f869c1517800a1547ab47
Pia <gayeongparkk@gmail.com>
2024-04-29 17:18:40 +0900

    feat: program hash in script dynamic

--- script/HdpExecutionStore.s.sol
+++ script/HdpExecutionStore.s.sol
@@ -13,15 +13,37 @@ contract HdpExecutionStoreDeployer is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        IFactsRegistry factsRegistry = IFactsRegistry(vm.envAddress("FACTS_REGISTRY_ADDRESS"));
-        IAggregatorsFactory aggregatorsFactory = IAggregatorsFactory(vm.envAddress("AGGREGATORS_FACTORY_ADDRESS"));
-        bytes32 programHash = vm.envBytes32("PROGRAM_HASH");
+        IFactsRegistry factsRegistry = IFactsRegistry(
+            vm.envAddress("FACTS_REGISTRY_ADDRESS")
+        );
+        IAggregatorsFactory aggregatorsFactory = IAggregatorsFactory(
+            vm.envAddress("AGGREGATORS_FACTORY_ADDRESS")
+        );
+        bytes32 programHash = _getProgramHash();
 
         // Deploy the HdpExecutionStore
-        HdpExecutionStore hdpExecutionStore = new HdpExecutionStore(factsRegistry, aggregatorsFactory, programHash);
+        HdpExecutionStore hdpExecutionStore = new HdpExecutionStore(
+            factsRegistry,
+            aggregatorsFactory,
+            programHash
+        );
 
-        console2.log("HdpExecutionStore deployed at: ", address(hdpExecutionStore));
+        console2.log(
+            "HdpExecutionStore deployed at: ",
+            address(hdpExecutionStore)
+        );
 
         vm.stopBroadcast();
+    }
+
+    function _getProgramHash() internal returns (bytes32) {
+        string[] memory inputs = new string[](5);
+        inputs[0] = "python3";
+        inputs[1] = "-m";
+        inputs[2] = "helpers.hash_program";
+        inputs[3] = "--program";
+        inputs[4] = "./helpers/target/hdp.json";
+        bytes memory abiEncoded = vm.ffi(inputs);
+        return abi.decode(abiEncoded, (bytes32));
     }
 }
