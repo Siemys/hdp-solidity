bd2694ca3608b4bf870e63c7bebd68624990fdc0
Pia <gayeongparkk@gmail.com>
2024-05-09 18:53:48 +0900

    fix scripts

--- helpers/script/fetch-input.sh
+++ helpers/script/fetch-input.sh
@@ -1,3 +1,21 @@
+#!/bin/bash
+
+prepare_cairo_enviroment() {
+    # Activate the virtual environment
+    source ./venv/bin/activate 
+    # Check if cairo-run is installed
+    cairo-run --version
+    local status=$?
+        if [ $status -eq 0 ]; then
+            echo "$(date '+%Y-%m-%d %H:%M:%S') - Successfully prepared"
+        else
+            echo "$(date '+%Y-%m-%d %H:%M:%S') - Failed to prepared"
+            return $status
+        fi
+}
+# Call the function to ensure the virtual environment is activated
+prepare_cairo_enviroment
+
 cairo-run \
   --program=helpers/target/hdp.json \
   --layout=starknet_with_keccak \
