d0cb3483b87e6c9cd765241a086f2ea8384c34a2
Pia <gayeongparkk@gmail.com>
2024-05-30 17:12:15 +0900

    fix: script function update

--- script/HdpExecutionStore.s.sol
+++ script/HdpExecutionStore.s.sol
@@ -13,14 +13,25 @@ contract HdpExecutionStoreDeployer is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        IFactsRegistry factsRegistry = IFactsRegistry(vm.envAddress("FACTS_REGISTRY_ADDRESS"));
-        IAggregatorsFactory aggregatorsFactory = IAggregatorsFactory(vm.envAddress("AGGREGATORS_FACTORY_ADDRESS"));
+        IFactsRegistry factsRegistry = IFactsRegistry(
+            vm.envAddress("FACTS_REGISTRY_ADDRESS")
+        );
+        IAggregatorsFactory aggregatorsFactory = IAggregatorsFactory(
+            vm.envAddress("AGGREGATORS_FACTORY_ADDRESS")
+        );
         bytes32 programHash = _getProgramHash();
 
         // Deploy the HdpExecutionStore
-        HdpExecutionStore hdpExecutionStore = new HdpExecutionStore(factsRegistry, aggregatorsFactory, programHash);
+        HdpExecutionStore hdpExecutionStore = new HdpExecutionStore(
+            factsRegistry,
+            aggregatorsFactory,
+            programHash
+        );
 
-        console2.log("HdpExecutionStore deployed at: ", address(hdpExecutionStore));
+        console2.log(
+            "HdpExecutionStore deployed at: ",
+            address(hdpExecutionStore)
+        );
 
         vm.stopBroadcast();
     }
@@ -31,7 +42,7 @@ contract HdpExecutionStoreDeployer is Script {
         inputs[1] = "-m";
         inputs[2] = "helpers.hash_program";
         inputs[3] = "--program";
-        inputs[4] = "./helpers/target/hdp.json";
+        inputs[4] = "build/compiled_cairo/hdp.json";
         bytes memory abiEncoded = vm.ffi(inputs);
         return abi.decode(abiEncoded, (bytes32));
     }
