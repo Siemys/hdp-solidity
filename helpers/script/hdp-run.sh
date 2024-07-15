bb1ee15ce8e96d3ebe533962da9fa06a4673dc08
Pia <gayeongparkk@gmail.com>
2024-07-15 19:59:57 +0200

    bump with new cli

--- helpers/script/hdp-run.sh
+++ helpers/script/hdp-run.sh
@@ -16,5 +16,5 @@ prepare_cairo_enviroment() {
 # Call the function to ensure the virtual environment is activated
 prepare_cairo_enviroment
 
-hdp encode -a -c helpers/target/bs_cached_input.json -o helpers/target/bs_cached_output.json -p helpers/target/bs_hdp_pie.zip slr none.10000000 -b 5858987 5858997 header.excess_blob_gas 2
-hdp encode -a -c helpers/target/tx_cached_input.json -o helpers/target/tx_cached_output.json -p helpers/target/tx_hdp_pie.zip slr none.50 -t 5605816 tx_receipt.success 12 53 1 0,0,1,1
+hdp encode -a -p helpers/target/bs_cached_input.json -o helpers/target/bs_cached_output.json -c helpers/target/bs_hdp_pie.zip avg -b 5858987 5858997 header.excess_blob_gas 2
+hdp encode -a -p helpers/target/tx_cached_input.json -o helpers/target/tx_cached_output.json -c helpers/target/tx_hdp_pie.zip avg -t 5605816 tx_receipt.success 12 53 1 0,0,1,1
