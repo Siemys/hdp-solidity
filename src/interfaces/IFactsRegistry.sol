baea47b15a3041eec5b0c6a923553378d606d277
Siemys <przemyslaw.siemaszko@gmail.com>
2024-07-26 12:49:38 +0200

    Update IFactsRegistry.sol

--- src/interfaces/IFactsRegistry.sol
+++ src/interfaces/IFactsRegistry.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.4;
 
 /// @notice Interface for the facts registry (https://github.com/starkware-libs/starkex-contracts/blob/master/scalable-dex/contracts/src/components/FactRegistry.sol)
